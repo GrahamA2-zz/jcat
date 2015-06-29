2265027e2b4ab3f5d8d21d459ee9cee6edc36be1
Graham Allan <graham.allan@bcs.org.uk>
2015-06-29 15:41:49 +0100

    added .io to package

--- src/main/java/io/hypercat/Application.java
+++ src/main/java/io/hypercat/Application.java
@@ -1,5 +1,7 @@
 package hypercat;
 
+import hypercat.Application;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
